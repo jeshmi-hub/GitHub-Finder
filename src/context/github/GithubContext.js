0f84ba94386fae0bbf54fd78480bf34fe90a0822
jeshmi-hub <rajakjeshmi19@gmail.com>
2023-05-11 16:45:22 +0545

    Search filter applied based on username

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,47 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext()
+
+
+
+export const GithubProvider = ({children}) =>{
+   const initialState = {
+    users: [],
+    loading: true
+   }
+
+   const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    const searchUsers = async(text)=>{
+        setLoading()
+        const params = new URLSearchParams({
+            q:text
+        })
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,{
+            headers:{
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        })
+
+        const {items} = await response.json()
+        dispatch({
+            type: 'GET_USERS',
+            payload: items,
+        })
+    }
+
+    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
+
+    return <GithubContext.Provider value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+    }}>
+        {children}</GithubContext.Provider>
+}
+
+export default GithubContext
